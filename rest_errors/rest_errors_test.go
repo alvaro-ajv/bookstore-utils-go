34d84f55c60e01d70d586d8336b321cfb201396d
Alvaro Vargas <alvaro.ajv@ibm.com>
2021-06-10 10:55:43 -0600

    rest_err

--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -10,9 +10,8 @@ import (
 func TestNewInternalServerError(t *testing.T) {
 	err := NewInternalServerError("this is the message", errors.New("database error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "internal_server_error", err.Error)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
 	assert.NotNil(t, err.Causes)
 	assert.EqualValues(t, 1, len(err.Causes()))
 	assert.EqualValues(t, "database error", err.Causes()[0])
