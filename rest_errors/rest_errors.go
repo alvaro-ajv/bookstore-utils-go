e5e9ce74bbbe55eef10364dd4ddfbfae8e1fe1eb
Alvaro Vargas <alvaro.ajv@ibm.com>
2021-06-10 10:01:26 -0600

    fixes

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -2,45 +2,86 @@ package rest_errors
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 )
 
-type RestError struct {
-	Message string `json:"message"`
-	Status  int    `json:"status"`
-	Error   string `json:"error"`
-	Causes []interface{} `json:"causes"`
+type RestError interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []interface{}
+}
+
+type restError struct {
+	message string `json:"message"`
+	status  int    `json:"status"`
+	error   string `json:"error"`
+	causes []interface{} `json:"causes"`
+}
+
+func (e restError) Error() string {
+	return fmt.Sprintf("message :%s - status: %d - error: %s - causes [ %v ]",
+		e.message, e.status, e.error, e.causes)
 }
 
 func NewError(msg string) error {
 	return errors.New(msg)
 }
 
-func NewBadRequestError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+func NewRestError(message string, status int, err string, causes []interface{}) RestError {
+	return restError{
+		message: message,
+		status: status,
+		error: err,
+		causes: causes,
 	}
 }
 
-func NewNotFoundError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
+func NewUnauthorizedError(message string) RestError {
+	return restError{
+		message: message,
+		status: http.StatusUnauthorized,
+		error: "unauthorized",
 	}
 }
 
-func NewInternalServerError(message string, err error) *RestError {
-	result := &RestError{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
+func NewBadRequestError(message string) RestError {
+	return &restError{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) RestError {
+	return &restError{
+		message: message,
+		status:  http.StatusNotFound,
+		error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string, err error) RestError {
+	result := &restError{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "internal_server_error",
 	}
 	if err != nil {
-		result.Causes = append(result.Causes, err.Error())
+		result.causes = append(result.causes, err.Error())
 	}
 	return result
 }
 
+func (e restError) Message() string {
+	return e.message
+}
+
+func (e restError) Status() int {
+	return e.status
+}
+
+func (e restError) Causes() []interface{} {
+	return e.causes
+}
